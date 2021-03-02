38678d4508c5ac54dec495a3838b4e3143be5b40
HoussamBedja <houssam.b@hotmail.com>
2021-03-02 18:27:04 -0500

    indenting code

--- load_chart.js
+++ load_chart.js
@@ -1,99 +1,109 @@
-
 Chart.plugins.unregister(ChartDataLabels);
 
 function drawDoughnutChart(targets_met, targets_total) {
-
-
   var data = {
     datasets: [
-        {
-            data: [targets_met/targets_total*100, (100-(targets_met/targets_total*100))],
-            backgroundColor: ["green","#D3D3D3"]
-        }]
-};
+      {
+        data: [
+          (targets_met / targets_total) * 100,
+          100 - (targets_met / targets_total) * 100,
+        ],
+        backgroundColor: ["green", "#D3D3D3"],
+      },
+    ],
+  };
 
-var ctx = document.getElementById("chart1");
+  var ctx = document.getElementById("chart1");
 
-// And for a doughnut chart
-var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
+  // And for a doughnut chart
+  var myDoughnutChart = new Chart(ctx, {
+    type: "doughnut",
     data: data,
     options: {
       legend: {
-        position: 'bottom',
+        position: "bottom",
         fullWidth: true,
         display: true,
         labels: {
           fontSize: 18,
-          fontColor: 'black'
-        }
+          fontColor: "black",
+        },
       },
       title: {
         display: true,
-        position: 'top',
+        position: "top",
         padding: 20,
         fontSize: 22,
-        color: 'black',
-        text: (fr_page) ? 'Rendement des Normes Relatives aux Services' : 'Service Standards Performance'
+        color: "black",
+        text: fr_page
+          ? "Rendement des Normes Relatives aux Services"
+          : "Service Standards Performance",
       },
-    	rotation: 1 * Math.PI,
+      rotation: 1 * Math.PI,
       circumference: 1 * Math.PI,
-      cutoutPercentage : 80,
+      cutoutPercentage: 80,
       tooltips: {
-         enabled: false
-       }
-    }
-});
+        enabled: false,
+      },
+    },
+  });
 
-  $('.donut-inner h2').text(targets_met + ' / ' + targets_total);
+  $(".donut-inner h2").text(targets_met + " / " + targets_total);
   if (targets_met > 1) {
-    (fr_page) ? $('#target').html('Les objectifs liés aux normes relatives aux services ont été atteints') : $('#target').html('service standard targets were met');
+    fr_page
+      ? $("#target").html(
+          "Les objectifs liés aux normes relatives aux services ont été atteints"
+        )
+      : $("#target").html("service standard targets were met");
   } else {
-    (fr_page) ? $('#target').html('L’objectif lié aux normes relatives aux services a été atteint') : $('#target').html('service standard target was met');
+    fr_page
+      ? $("#target").html(
+          "L’objectif lié aux normes relatives aux services a été atteint"
+        )
+      : $("#target").html("service standard target was met");
   }
-
 }
 
-
 function drawBarChart(serviceSums, yr_labels) {
-
   var ctx = document.getElementById("chart2");
   ctx.height = 54;
-  maxYearSum = _.max(serviceSums)
+  maxYearSum = _.max(serviceSums);
 
   var data = {
     labels: yr_labels,
     datasets: [
-        {
-            data: serviceSums,
-            backgroundColor: "#006699",
-        }]
+      {
+        data: serviceSums,
+        backgroundColor: "#006699",
+      },
+    ],
   };
 
-
   var options = {
-
     title: {
       display: true,
-      text: (fr_page) ? 'Nombre Total d\'Applications' : 'Total Number of Applications',
-      position: 'top',
+      text: fr_page
+        ? "Nombre Total d'Applications"
+        : "Total Number of Applications",
+      position: "top",
       padding: 20,
       fontSize: 22,
       // fontColor: 'black'
     },
     legend: {
       display: false,
-      position: 'bottom',
+      position: "bottom",
       onClick: function (e) {
         e.stopPropagation();
-      }
-    },
-      animation: {
-          duration: 1500
       },
-      devicePixelRatio: 1.5,
-      scales: {
-        xAxes: [{
+    },
+    animation: {
+      duration: 1500,
+    },
+    devicePixelRatio: 1.5,
+    scales: {
+      xAxes: [
+        {
           barPercentage: 0.8,
           categoryPercentage: 1,
           gridLines: {
@@ -101,11 +111,13 @@ function drawBarChart(serviceSums, yr_labels) {
             drawBorder: false,
           },
           ticks: {
-            fontColor: 'black',
-            fontSize: 16
+            fontColor: "black",
+            fontSize: 16,
           },
-        }],
-        yAxes: [{
+        },
+      ],
+      yAxes: [
+        {
           gridLines: {
             display: false,
             drawBorder: false,
@@ -113,29 +125,30 @@ function drawBarChart(serviceSums, yr_labels) {
           ticks: {
             display: false,
             suggestedMin: 0,
-            suggestedMax: (maxYearSum*1.1),
+            suggestedMax: maxYearSum * 1.1,
           },
-        }],
+        },
+      ],
+    },
+    plugins: {
+      datalabels: {
+        // color: 'black',
+        anchor: "end",
+        align: "top",
+        font: {
+          size: "20",
+        },
+        formatter: function (value, context) {
+          return formatNumberMini(value);
+        },
       },
-      plugins: {
-        datalabels: {
-          // color: 'black',
-          anchor: 'end',
-          align: 'top',
-          font: {
-            size: '20'
-          },
-          formatter: function(value, context) {
-            return formatNumberMini(value);
-          }
-        }
-      }
-  }
+    },
+  };
 
   var myBarChart = new Chart(ctx, {
     plugins: [ChartDataLabels],
-    type: 'bar',
+    type: "bar",
     data: data,
-    options: options
+    options: options,
   });
 }
--- load_data.js
+++ load_data.js
@@ -1,20 +1,24 @@
 //
 // let formatDollar = function(d) { return "$" + d3.format(",.0f")(d).replace(/G/,"B"); }
 // let formatDollarMini = function(d) { return "$" + d3.format(".2s")(d).replace(/G/,"B"); }
-let formatPercentDecimal = function(d) { return d3.format(".1f")(d) + "%"; }
-let formatPercent = function(d) { return d3.format(".0f")(d) + "%"; }
-let formatNumberMini = function(d) { 
-  if (d<10) {
-    return d3.format(".1s")(d).replace(/G/,"B");
+let formatPercentDecimal = function (d) {
+  return d3.format(".1f")(d) + "%";
+};
+let formatPercent = function (d) {
+  return d3.format(".0f")(d) + "%";
+};
+let formatNumberMini = function (d) {
+  if (d < 10) {
+    return d3.format(".1s")(d).replace(/G/, "B");
   }
-  if (d<100) {
-    return d3.format(".2s")(d).replace(/G/,"B");
+  if (d < 100) {
+    return d3.format(".2s")(d).replace(/G/, "B");
   }
-  return d3.format(".3s")(d).replace(/G/,"B");
-}
+  return d3.format(".3s")(d).replace(/G/, "B");
+};
 
 var url = window.location.href;
-var service_id = decodeURIComponent(url.split('?').pop());
+var service_id = decodeURIComponent(url.split("?").pop());
 
 // Testing variables
 // var url = 'search.open.canada.ca/chart/si/index-en.html?135';
@@ -22,194 +26,291 @@ var service_id = decodeURIComponent(url.split('?').pop());
 
 var fr_page = false;
 
-if (url.indexOf('index-fr.html') > -1) {
+if (url.indexOf("index-fr.html") > -1) {
   fr_page = true;
-  $('#toggle').attr('href','index-en.html?' + service_id);
+  $("#toggle").attr("href", "index-en.html?" + service_id);
 } else {
-  $('#toggle').attr('href','index-fr.html?' + service_id);
+  $("#toggle").attr("href", "index-fr.html?" + service_id);
 }
 
 function sumTransactions(service) {
-  var online_applications = (service[0]['online_applications'] == ("" || "ND")) ? 0 : parseInt(service[0]['online_applications']);
-  var in_person_applications = (service[0]['in_person_applications'] == ("" || "ND")) ? 0 : parseInt(service[0]['in_person_applications']);
-  var telephone_applications = (service[0]['telephone_applications'] == ("" || "ND")) ? 0 : parseInt(service[0]['telephone_applications']);
-  var other_applications = (service[0]['other_applications'] == ("" || "ND")) ? 0 : parseInt(service[0]['other_applications']);
-  var postal_mail_applications = (service[0]['postal_mail_applications'] == ("" || "ND")) ? 0 : parseInt(service[0]['postal_mail_applications']);
-  return online_applications + in_person_applications + telephone_applications + other_applications + postal_mail_applications;
+  var online_applications =
+    service[0]["online_applications"] == ("" || "ND")
+      ? 0
+      : parseInt(service[0]["online_applications"]);
+  var in_person_applications =
+    service[0]["in_person_applications"] == ("" || "ND")
+      ? 0
+      : parseInt(service[0]["in_person_applications"]);
+  var telephone_applications =
+    service[0]["telephone_applications"] == ("" || "ND")
+      ? 0
+      : parseInt(service[0]["telephone_applications"]);
+  var other_applications =
+    service[0]["other_applications"] == ("" || "ND")
+      ? 0
+      : parseInt(service[0]["other_applications"]);
+  var postal_mail_applications =
+    service[0]["postal_mail_applications"] == ("" || "ND")
+      ? 0
+      : parseInt(service[0]["postal_mail_applications"]);
+  return (
+    online_applications +
+    in_person_applications +
+    telephone_applications +
+    other_applications +
+    postal_mail_applications
+  );
 }
 
 function consumeData(error, services_data, standards_data) {
-
-  if (error){
-      console.log("Error on data load");
+  if (error) {
+    console.log("Error on data load");
   }
 
-  var year_data =  _.chain(services_data).groupBy('fiscal_yr').value();
+  var year_data = _.chain(services_data).groupBy("fiscal_yr").value();
 
-  var service_17_18 = _.filter(year_data['2017-2018'], function (row) {
-    return !isNaN(row['service_id']) && service_id === row['service_id'];
+  var service_17_18 = _.filter(year_data["2017-2018"], function (row) {
+    return !isNaN(row["service_id"]) && service_id === row["service_id"];
   });
 
-  var service_16_17 = _.filter(year_data['2016-2017'], function (row) {
-    return !isNaN(row['service_id']) && service_id === row['service_id'];
+  var service_16_17 = _.filter(year_data["2016-2017"], function (row) {
+    return !isNaN(row["service_id"]) && service_id === row["service_id"];
   });
 
-  var service_18_19 = _.filter(year_data['2018-2019'], function (row) {
-    return !isNaN(row['service_id']) && service_id === row['service_id'];
+  var service_18_19 = _.filter(year_data["2018-2019"], function (row) {
+    return !isNaN(row["service_id"]) && service_id === row["service_id"];
   });
 
-  var service_19_20 = _.filter(year_data['2019-2020'], function (row) {
-    return !isNaN(row['service_id']) && service_id === row['service_id'];
+  var service_19_20 = _.filter(year_data["2019-2020"], function (row) {
+    return !isNaN(row["service_id"]) && service_id === row["service_id"];
   });
 
-  console.log({service_16_17,service_17_18,service_18_19,service_19_20});
+  console.log({ service_16_17, service_17_18, service_18_19, service_19_20 });
 
   // Sum of transactions
-  var sum_transactions_16_17 = (service_16_17.length > 0) ? sumTransactions(service_16_17) : 0;
-  var sum_transactions_17_18 = (service_17_18.length > 0) ? sumTransactions(service_17_18) : 0;
-  var sum_transactions_18_19 = (service_18_19.length > 0) ? sumTransactions(service_18_19) : 0;
-  var sum_transactions_19_20 = (service_19_20.length > 0) ? sumTransactions(service_19_20) : 0;
-  var service = (service_19_20.length > 0) ? service_19_20 : (service_18_19.length > 0) ? service_18_19 : (service_17_18.length > 0) ? service_17_18: service_16_17;
-  
-  console.log(service)
+  var sum_transactions_16_17 =
+    service_16_17.length > 0 ? sumTransactions(service_16_17) : 0;
+  var sum_transactions_17_18 =
+    service_17_18.length > 0 ? sumTransactions(service_17_18) : 0;
+  var sum_transactions_18_19 =
+    service_18_19.length > 0 ? sumTransactions(service_18_19) : 0;
+  var sum_transactions_19_20 =
+    service_19_20.length > 0 ? sumTransactions(service_19_20) : 0;
+  var service =
+    service_19_20.length > 0
+      ? service_19_20
+      : service_18_19.length > 0
+      ? service_18_19
+      : service_17_18.length > 0
+      ? service_17_18
+      : service_16_17;
+
+  console.log(service);
   function drawChart2() {
-    var serviceSum = []
-    var labels = []
+    var serviceSum = [];
+    var labels = [];
     if (sum_transactions_16_17 > 0) {
       serviceSum.push(sum_transactions_16_17);
-      labels.push('2016-17')
+      labels.push("2016-17");
     }
     if (sum_transactions_17_18 > 0) {
       serviceSum.push(sum_transactions_17_18);
-      labels.push('2017-18')
+      labels.push("2017-18");
     }
     if (sum_transactions_18_19 > 0) {
       serviceSum.push(sum_transactions_18_19);
-      labels.push('2018-19')
+      labels.push("2018-19");
     }
     if (sum_transactions_19_20 > 0) {
       serviceSum.push(sum_transactions_19_20);
-      labels.push('2019-20')
+      labels.push("2019-20");
     }
     drawBarChart(serviceSum, labels);
   }
 
-
   //online percentage
-  if ((service[0]['online_applications'] != ("" || "ND")) && sumTransactions(service) > 0) {
-    var online_applications = parseInt(service[0]['online_applications']);
-    var online_percent = 100*online_applications/sumTransactions(service)
+  if (
+    service[0]["online_applications"] != ("" || "ND") &&
+    sumTransactions(service) > 0
+  ) {
+    var online_applications = parseInt(service[0]["online_applications"]);
+    var online_percent = (100 * online_applications) / sumTransactions(service);
     console.log(online_percent);
-    $('#online_percent').html(formatPercent(online_percent));
-    $('#online_percent_section').show();
+    $("#online_percent").html(formatPercent(online_percent));
+    $("#online_percent_section").show();
   }
-  
-
 
   //Append service title & description
   if (fr_page) {
-    $('h1').html(service[0]['service_name_fr'] + ': Tableau de bord sur le rendement');
-    $('#service_title').html('<b>Titre du service</b> : ' + service[0]['service_name_fr']);
-    var org_name = service[0]['department_name_en'];
-    $('#service_department').html('<b>Ministère</b> : ' + org_name);
-    $('#service_description').html('<b>Description du service</b> : ' + service[0]['service_description_fr']);
-    $('#service_year').html('<b>Année de la déclaration</b> : ' + service[0]['fiscal_yr']);
-    $('#service_fee').html('<b>Frais de service</b> : ' + ((service[0]['service_fee'] == 'Y') ? 'Ce service comporte des frais de service.' : 'Ce service ne comporte aucun frais de service.'));
-    if (validURL(service[0]['service_url_fr'])) {
-      $('#service_url').html('<b>Lien au service</b> : <a class="btn btn-default" href="'+ service[0]['service_url_fr'] +'">Accedez ici</a>');
+    $("h1").html(
+      service[0]["service_name_fr"] + ": Tableau de bord sur le rendement"
+    );
+    $("#service_title").html(
+      "<b>Titre du service</b> : " + service[0]["service_name_fr"]
+    );
+    var org_name = service[0]["department_name_en"];
+    $("#service_department").html("<b>Ministère</b> : " + org_name);
+    $("#service_description").html(
+      "<b>Description du service</b> : " + service[0]["service_description_fr"]
+    );
+    $("#service_year").html(
+      "<b>Année de la déclaration</b> : " + service[0]["fiscal_yr"]
+    );
+    $("#service_fee").html(
+      "<b>Frais de service</b> : " +
+        (service[0]["service_fee"] == "Y"
+          ? "Ce service comporte des frais de service."
+          : "Ce service ne comporte aucun frais de service.")
+    );
+    if (validURL(service[0]["service_url_fr"])) {
+      $("#service_url").html(
+        '<b>Lien au service</b> : <a class="btn btn-default" href="' +
+          service[0]["service_url_fr"] +
+          '">Accedez ici</a>'
+      );
     }
   } else {
-    $('h1').html(service[0]['service_name_en'] + ': Performance Dashboard');
-    $('#service_title').html('<b>Service name</b>: ' + service[0]['service_name_en']);
-    var org_name = service[0]['department_name_en'];
-    $('#service_department').html('<b>Department</b>: ' + org_name);
-    $('#service_description').html('<b>Service description</b>: ' + service[0]['service_description_en']);
-    $('#service_year').html('<b>Year reported</b>: ' + service[0]['fiscal_yr']);
-    $('#service_fee').html('<b>Service fees</b>: ' + ((service[0]['service_fee'] == 'Y') ? 'This service has service fees.' : 'This service does not have any service fees.'));
-    if (validURL(service[0]['service_url_en'])) {
-      $('#service_url').html('<b>Link to service</b> : <a class="btn btn-default" href="'+ service[0]['service_url_en'] +'">Access here</a>');
+    $("h1").html(service[0]["service_name_en"] + ": Performance Dashboard");
+    $("#service_title").html(
+      "<b>Service name</b>: " + service[0]["service_name_en"]
+    );
+    var org_name = service[0]["department_name_en"];
+    $("#service_department").html("<b>Department</b>: " + org_name);
+    $("#service_description").html(
+      "<b>Service description</b>: " + service[0]["service_description_en"]
+    );
+    $("#service_year").html("<b>Year reported</b>: " + service[0]["fiscal_yr"]);
+    $("#service_fee").html(
+      "<b>Service fees</b>: " +
+        (service[0]["service_fee"] == "Y"
+          ? "This service has service fees."
+          : "This service does not have any service fees.")
+    );
+    if (validURL(service[0]["service_url_en"])) {
+      $("#service_url").html(
+        '<b>Link to service</b> : <a class="btn btn-default" href="' +
+          service[0]["service_url_en"] +
+          '">Access here</a>'
+      );
     }
   }
 
   // e-enablement
 
   var e_enablement = {
-    'e_registration' : service[0]['e_registration'],
-    'e_authentication' : service[0]['e_authentication'],
-    'e_application' : service[0]['e_application'],
-    'e_decision' : service[0]['e_decision'],
-    'e_issuance' : service[0]['e_issuance'],
-    'e_feedback' : service[0]['e_feedback']
+    e_registration: service[0]["e_registration"],
+    e_authentication: service[0]["e_authentication"],
+    e_application: service[0]["e_application"],
+    e_decision: service[0]["e_decision"],
+    e_issuance: service[0]["e_issuance"],
+    e_feedback: service[0]["e_feedback"],
   };
   loadEnablementTable(e_enablement);
 
   //Service fee
-  var service_fee = service[0]['e_feedback'];
-
-
+  var service_fee = service[0]["e_feedback"];
 
   //target data
-  var standards = _.filter(standards_data, function(row) {
-    return (service[0]['service_id'] === row['service_id']) && (service[0]['fiscal_yr'] === row['fiscal_yr']);
+  var standards = _.filter(standards_data, function (row) {
+    return (
+      service[0]["service_id"] === row["service_id"] &&
+      service[0]["fiscal_yr"] === row["fiscal_yr"]
+    );
   });
-  console.log(standards)
+  console.log(standards);
   function drawChart1() {
-    if(standards.length > 0) {
-      var targets_met = _.filter(standards, function(obj) { return parseFloat(obj['performance']) >= parseFloat(obj['service_std_target']) });
-      var avrg_target = _.reduce(_.pluck(standards, 'service_std_target'), function(memo, num) { return memo + parseFloat(num)},0)/standards.length;
-      var avrg_performance = _.reduce(_.pluck(standards, 'performance'), function(memo, num) { return memo + parseFloat(num)},0)/standards.length;
+    if (standards.length > 0) {
+      var targets_met = _.filter(standards, function (obj) {
+        return (
+          parseFloat(obj["performance"]) >=
+          parseFloat(obj["service_std_target"])
+        );
+      });
+      var avrg_target =
+        _.reduce(
+          _.pluck(standards, "service_std_target"),
+          function (memo, num) {
+            return memo + parseFloat(num);
+          },
+          0
+        ) / standards.length;
+      var avrg_performance =
+        _.reduce(
+          _.pluck(standards, "performance"),
+          function (memo, num) {
+            return memo + parseFloat(num);
+          },
+          0
+        ) / standards.length;
       drawDoughnutChart(targets_met.length, standards.length);
     } else {
-      $('#standards').attr('style','display: none');
-      (fr_page) ? $('#standards-note').html('Note: Aucuns standards de service ont été collectés pour ce service.') : $('#standards-note').html('Note: No service standard information was collected for this service.');
+      $("#standards").attr("style", "display: none");
+      fr_page
+        ? $("#standards-note").html(
+            "Note: Aucuns standards de service ont été collectés pour ce service."
+          )
+        : $("#standards-note").html(
+            "Note: No service standard information was collected for this service."
+          );
     }
   }
 
-
   // Populate service standards table
-  var tableData = _.map(standards, function(standard) {
-    if(fr_page) {
+  var tableData = _.map(standards, function (standard) {
+    if (fr_page) {
       var tableFormat = {
-        'Norme relative aux services' : standard.service_std_fr,
-        'Objectif' : (standard.service_std_target != '') ? formatPercentDecimal(parseFloat(standard.service_std_target)) : '',
-        'Résultat' : (standard.performance != 'ND') ? formatPercentDecimal(parseFloat(standard.performance)) : 'ND'
+        "Norme relative aux services": standard.service_std_fr,
+        Objectif:
+          standard.service_std_target != ""
+            ? formatPercentDecimal(parseFloat(standard.service_std_target))
+            : "",
+        Résultat:
+          standard.performance != "ND"
+            ? formatPercentDecimal(parseFloat(standard.performance))
+            : "ND",
       };
     } else {
       var tableFormat = {
-        'Service standard' : standard.service_std_en,
-        'Target' : (standard.service_std_target != '') ? formatPercentDecimal(parseFloat(standard.service_std_target)) : '',
-        'Result' : (standard.performance != 'ND') ? formatPercentDecimal(parseFloat(standard.performance)) : 'ND'
+        "Service standard": standard.service_std_en,
+        Target:
+          standard.service_std_target != ""
+            ? formatPercentDecimal(parseFloat(standard.service_std_target))
+            : "",
+        Result:
+          standard.performance != "ND"
+            ? formatPercentDecimal(parseFloat(standard.performance))
+            : "ND",
       };
     }
     return tableFormat;
   });
   populateTable(tableData);
 
-
   // Animate Chart drawing using Materialize
   var options = [
-    {selector: '#chart2', offset:50, callback: drawChart2},
-    {selector: '#chart1', offset:50, callback: drawChart1}
+    { selector: "#chart2", offset: 50, callback: drawChart2 },
+    { selector: "#chart1", offset: 50, callback: drawChart1 },
   ];
   Materialize.scrollFire(options);
-
 }
 
 function validURL(str) {
-  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
   return !!pattern.test(str);
 }
 
-
-
 d3.queue()
-  .defer(d3.csv, 'service_inventory.csv')
-  .defer(d3.csv, 'service_standards.csv')
+  .defer(d3.csv, "service_inventory.csv")
+  .defer(d3.csv, "service_standards.csv")
   // .defer(d3.csv, 'https://open.canada.ca/data/dataset/3ac0d080-6149-499a-8b06-7ce5f00ec56c/resource/3acf79c0-a5f5-4d9a-a30d-fb5ceba4b60a/download/service.csv')
   // .defer(d3.csv, 'https://open.canada.ca/data/dataset/3ac0d080-6149-499a-8b06-7ce5f00ec56c/resource/272143a7-533e-42a1-b72d-622116474a21/download/service-std.csv')
   .await(consumeData); //only function name is needed
